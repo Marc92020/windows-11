ac29364c5a3c61d6e2c2e4b9afd95e1e379c80a8
LankyBox01 <79767244+LankyBox01@users.noreply.github.com>
2021-08-18 17:02:17 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
-document.getElementById("logostart_NXJH").addEventListener("click", function() {
-  document.getElementById("startmenu_MMIA").style.opacity = 1;
+document.body.addEventListener("click", function(targ) {
+  if (targ.target.id != 'logostart_NXJH') {
+        document.getElementById("startmenu_MMIA").style.opacity = 0;
+  }else{
+        document.getElementById("startmenu_MMIA").style.opacity = 1;
+  }
 });
