9dd56bf40fbf8934933b9aa2363b73c029818a9f
LankyBox01 <79767244+LankyBox01@users.noreply.github.com>
2021-08-18 16:50:45 +0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("startmenu_MMIA").style.display = "none";
+
+document.getElementById("logostart_NXJH").addEventListener("click", function() {
+  document.getElementById("startmenu_MMIA").style.display = "initial";
+});
