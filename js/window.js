407c810410410f019a2f71975f389bd4cf9b3206
LankyBox01 <79767244+LankyBox01@users.noreply.github.com>
2021-08-20 11:28:04 +0300

    Update window.js

--- js/window.js
+++ js/window.js
@@ -1,6 +1,6 @@
 fetch('info.txt')
   .then(response => response.text())
-  .then(data => document.getElementById("mydiv").insertAdjacentHTML("beforeEnd", data.replace("\n", "<br>")));
+  .then(data => document.getElementById("mydiv").insertAdjacentHTML("beforeEnd", data.replaceAll("\n", "<br>")));
 
 //Make the DIV element draggagle:
 dragElement(document.getElementById("mydiv"));
