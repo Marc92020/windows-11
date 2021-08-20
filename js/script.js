9f7d4a4a85a4d5d55e7c1e33fb9b2529cce4cdf8
LankyBox01 <79767244+LankyBox01@users.noreply.github.com>
2021-08-20 12:11:27 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 document.getElementById("logostart_NXJH").addEventListener("click", function() {
   if (document.getElementById("startmenu_MMIA").style.opacity == 1){
    document.getElementById("startmenu_MMIA").style.transition = "1s";
-   document.getElementById("startmenu_MMIA").style.zIndex = "0";
+   document.getElementById("startmenu_MMIA").style.zIndex = 0;
    document.getElementById("startmenu_MMIA").style.opacity = 0;
   }else{
    document.getElementById("startmenu_MMIA").style.transition = ".2s";
-   document.getElementById("startmenu_MMIA").style.zIndex = "1000";
+   document.getElementById("startmenu_MMIA").style.zIndex = 1000;
    document.getElementById("startmenu_MMIA").style.opacity = 1;
   }
 });
